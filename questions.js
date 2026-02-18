06e3cbfe3346f91f0b899d9e74457995de5b1b5c
Sharath Swaroop Natram <58197618+NatramS@users.noreply.github.com>
2026-02-18 12:24:32 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,24 @@
+const questions = [
+  {
+    topic: "Cloud",
+    question: "What is AWS EC2?",
+    options: [
+      "Storage service",
+      "Compute service",
+      "Database service",
+      "Networking service"
+    ],
+    answer: 1
+  },
+  {
+    topic: "DevOps",
+    question: "Which tool is used for CI/CD?",
+    options: [
+      "Jenkins",
+      "Docker",
+      "Linux",
+      "Oracle"
+    ],
+    answer: 0
+  }
+];
